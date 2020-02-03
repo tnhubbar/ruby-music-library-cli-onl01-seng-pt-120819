a0e825337b95bbe9a4f1e5116e8907b2b9146724
Taryn Johnson <taryn.johnson919@gmail.com>
2020-02-03 01:24:48 +0000

    Automatically backed up by Learn

--- lib/MusicLibraryController.rb
+++ lib/MusicLibraryController.rb
@@ -55,4 +55,15 @@ class MusicLibraryController
     end
   end
   
+  def list_songs_by_artist
+    puts "Please enter the name of an artist:"
+    input = gets.strip
+
+    if artist = Artist.find_by_name(input)
+      artist.songs.sort{ |a, b| a.name <=> b.name }.each.with_index(1) do |s, i|
+        puts "#{i}. #{s.name} - #{s.genre.name}"
+      end
+    end
+  end
+  
 end 
