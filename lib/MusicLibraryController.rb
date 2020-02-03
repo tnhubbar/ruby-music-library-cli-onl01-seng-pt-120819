8904338ddfae4678ebdf7d96ba00b7cffd63e6cb
Taryn Johnson <taryn.johnson919@gmail.com>
2020-02-03 01:24:23 +0000

    Automatically backed up by Learn

--- lib/MusicLibraryController.rb
+++ lib/MusicLibraryController.rb
@@ -49,6 +49,10 @@ class MusicLibraryController
     end
   end
   
-  
+  def list_genres
+    Genre.all.sort{ |a, b| a.name <=> b.name }.each.with_index(1) do |g, i|
+      puts "#{i}. #{g.name}"
+    end
+  end
   
 end 
